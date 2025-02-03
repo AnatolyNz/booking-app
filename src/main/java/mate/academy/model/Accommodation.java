5a4b83763522b7b140a24ba841ec12823f6650f7
Anatolii Nazarov <anatoly_n@ukr.net>
2025-02-03 10:44:33 +0200

    Added entity Accommodation

---
+++ src/main/java/mate/academy/model/Accommodation.java
@@ -0,0 +1,67 @@
+package mate.academy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.Data;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE accommodations SET is_deleted = true WHERE id=?")
+@FilterDef(name = "softDeleteFilter", parameters = @ParamDef(name = "isDeleted",
+        type = Boolean.class))
+@Filter(name = "softDeleteFilter", condition = "is_deleted = :isDeleted")
+@Table(name = "accommodations")
+public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "accommodation")
+    private List<Booking> bookings;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String size;
+
+    @NotEmpty
+    private List<String> amenities;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer availability;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    @Column
+    private BigDecimal dailyRate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+
+    public enum Type {
+        HOUSE,
+        APARTMENT,
+        CONDO,
+        VACATION_HOME
+    }
+}

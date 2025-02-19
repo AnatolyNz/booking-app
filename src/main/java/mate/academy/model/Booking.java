f3dc11a01f2a2e4943c372ba64d7473174bb1fa9
Anatolii Nazarov <anatoly_n@ukr.net>
2025-02-19 11:58:02 +0200

    Added booking entity

--- src/main/java/mate/academy/model/Booking.java
+++ src/main/java/mate/academy/model/Booking.java
@@ -1,7 +1,43 @@
 package mate.academy.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.time.LocalDate;
 
 @Entity
+@Data
+@Table(name = "booking")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    @ManyToOne
+    @JoinColumn(name = "accommodation_id")
+    private Accommodation accommodation;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    public enum BookingStatus {
+        PENDING,
+        CONFIRMED,
+        CANCELED,
+        EXPIRED
+    }
 }

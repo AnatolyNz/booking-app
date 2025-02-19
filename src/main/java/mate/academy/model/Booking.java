477d6c3511c993e2f4b0ce3a7f62c006a79d1109
Anatolii Nazarov <anatoly_n@ukr.net>
2025-02-19 20:11:23 +0200

    Changed booking entity

--- src/main/java/mate/academy/model/Booking.java
+++ src/main/java/mate/academy/model/Booking.java
@@ -1,37 +1,50 @@
 package mate.academy.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
 import java.time.LocalDate;
+import lombok.Data;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE accommodations SET is_deleted = true WHERE id=?")
+@FilterDef(name = "softDeleteFilter", parameters = @ParamDef(name = "isDeleted",
+        type = Boolean.class))
+@Filter(name = "softDeleteFilter", condition = "is_deleted = :isDeleted")
 @Table(name = "booking")
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private LocalDate checkInDate;
+
+    @Column(nullable = false)
     private LocalDate checkOutDate;
 
     @ManyToOne
-    @JoinColumn(name = "accommodation_id")
+    @JoinColumn(name = "accommodation_id", nullable = false)
     private Accommodation accommodation;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private BookingStatus status;
 
     public enum BookingStatus {

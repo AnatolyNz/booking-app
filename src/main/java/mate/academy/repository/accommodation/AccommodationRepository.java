62ba64de4083c02e4ff6768d8a83089a3b19be3b
Anatolii Nazarov <anatoly_n@ukr.net>
2025-02-26 14:51:13 +0200

    Added JWT token authentication for users service_2

--- src/main/java/mate/academy/repository/accommodation/AccommodationRepository.java
+++ src/main/java/mate/academy/repository/accommodation/AccommodationRepository.java
@@ -2,7 +2,7 @@ package mate.academy.repository.accommodation;
 
 import java.util.List;
 import java.util.Optional;
-import mate.academy.model.Accommodation;
+import mate.academy.exception.model.Accommodation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;

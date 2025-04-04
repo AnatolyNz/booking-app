13ae305d116084c8d6c63b33e41577948dab049b
Anatolii Nazarov <anatoly_n@ukr.net>
2025-04-04 17:34:24 +0300

    Added Payment table into DB and Payment entity

---
+++ src/main/java/mate/academy/model/Payment.java
@@ -0,0 +1,56 @@
+package mate.academy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Data;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE payments SET is_deleted = true WHERE id=?")
+@FilterDef(name = "paymentSoftDeleteFilter", parameters = @ParamDef(name = "isDeleted",
+        type = Boolean.class))
+@Filter(name = "paymentSoftDeleteFilter", condition = "is_deleted = :isDeleted")
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(nullable = false)
+    private String sessionUrl;
+
+    @Column(nullable = false, unique = true)
+    private String sessionId;
+
+    @Column(nullable = false)
+    private BigDecimal amountToPay;
+
+    public enum PaymentStatus {
+        PENDING,
+        PAID,
+        FAILED,
+        CANCELED
+    }
+}

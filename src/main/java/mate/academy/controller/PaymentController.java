89510a2867f58422c31c25f451ba71ae25000238
Anatolii Nazarov <anatoly_n@ukr.net>
2025-04-05 11:10:07 +0300

    Added fixed PaymentController

--- src/main/java/mate/academy/controller/PaymentController.java
+++ src/main/java/mate/academy/controller/PaymentController.java
@@ -1,8 +1,9 @@
 package mate.academy.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import mate.academy.service.PaymentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,16 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Payment management", description = "Endpoints for managing payment")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
 
     private final PaymentService paymentService;
-
-    @Autowired
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
 
     @GetMapping
     public Map<String, Object> getPaymentInformation(@RequestParam("user_id") Long userId) {
